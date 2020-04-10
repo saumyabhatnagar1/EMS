1665667d31b9e887c503e442fabf6ccf7aaa061f
Naveen <naveenyadav6256@gmail.com>
2020-04-10 10:09:32 +0530

    [Naveen] Refactor. removes commnented code from navbar and profile component

--- ui/src/app/profile/profile.component.ts
+++ ui/src/app/profile/profile.component.ts
@@ -12,13 +12,8 @@ import { Router} from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   public message:any;
-  // public loginForm = new FormGroup({
-  //     email : new FormControl('',Validators.email),
-  //     // password: new FormControl('',Validators.required)
-  // });
 
   constructor(private profileService:ProfileService, private principle:PrincipleService,private router:Router) { }
-   userData
   ngOnInit(): void {
        this.get_profile();
   }
@@ -32,9 +27,6 @@ get_profile(){
   let user_login_json = JSON.stringify(data);
   this.profileService.get_profile(user_login_json).subscribe(
   res=>{
-
-        // this.principle.setUser(res);
-        // window.location.href = '/';
         console.log(res)
         this.message =res;
   },
