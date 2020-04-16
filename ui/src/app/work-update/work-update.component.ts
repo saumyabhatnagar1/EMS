a6df13998ada548ef4f476a3ec868a4723e2d285
saumyabhatnagar1 <bhatnagar.saumya2000@gmail.com>
2020-04-16 22:54:10 +0530

    [Saumya] Fix. Task and project objects and serparated

--- ui/src/app/work-update/work-update.component.ts
+++ ui/src/app/work-update/work-update.component.ts
@@ -6,15 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-update.component.css']
 })
 export class WorkUpdateComponent implements OnInit {
-   page:number =1;
-   count:number=0;
-   count2:number=0;
-   count3:number=0;
-  show:boolean=false;
+  page: number = 1;
+  count: number = 0;
+  count2: number = 0;
+  count3: number = 0;
+  show: boolean = false;
 
-   constructor() { }
-   ngOnInit(): void {
-     
+  constructor() { }
+  ngOnInit(): void {
+
   }
 
 
@@ -26,130 +26,143 @@ export class WorkUpdateComponent implements OnInit {
         member1: { name: "Naveen", role: "RMN" },
         member2: { name: "Raj", role: "Developer" }
       },
-      tasks:[ {
-        taskid: 100,
-        assignee: 'Admin',
-        dueDate: '20-10-200',
-        status: 'done',
-        desc:'Fill the form'
 
-      }, {
-        taskid:101,
-        assignee:'Admin',
-        dueDate:'25-10-2020',
-        status:'not completed',
-        desc:'COmplete the code'
-
-      }
-      ],
       Date: "September 6,2020"
     },
     {
-    id : 2,
-    name : "ABC",
-    team : {member1 : {name:"Abdul",role:"Developer"},
-            member2 : {name:"Sarthak",role:"Developer"}
-            },
-            tasks:[ {
-              taskid: 200,
-              assignee: 'Admin',
-              dueDate: '20-10-200',
-              status: 'done',
-              desc:'water the plants'
-      
-            }, {
-              taskid:201,
-              assignee:'Admin',
-              dueDate:'25-10-2020',
-              status:'not completed',
-              desc:'Wash the car'
-      
-            }
-            ],            
-    Date : "September 7,2020"
-  },
-  {
-    id : 3,
-    name : "DEF",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 4,
-    name : "GHI",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 5,
-    name : "JKL",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 6,
-    name : "MNO",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 7,
-    name : "PQR",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 8,
-    name : "STU",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 9,
-    name : "VWX",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 10,
-    name : "YZA",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 11,
-    name : "BCD",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-  {
-    id : 12,
-    name : "EFG",
-    team : {member1 : {name:"Saumya",role:"Developer"},
-            member2 : {name:"Rishabh",role:"Developer"}
-            },
-    Date : "September 8,2020"
-  },
-]
+      id: 2,
+      name: "ABC",
+      team: {
+        member1: { name: "Abdul", role: "Developer" },
+        member2: { name: "Sarthak", role: "Developer" }
+      },
 
+      Date: "September 7,2020"
+    },
+    {
+      id: 3,
+      name: "DEF",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 4,
+      name: "GHI",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 5,
+      name: "JKL",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 6,
+      name: "MNO",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 7,
+      name: "PQR",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 8,
+      name: "STU",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 9,
+      name: "VWX",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 10,
+      name: "YZA",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 11,
+      name: "BCD",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+    {
+      id: 12,
+      name: "EFG",
+      team: {
+        member1: { name: "Saumya", role: "Developer" },
+        member2: { name: "Rishabh", role: "Developer" }
+      },
+      Date: "September 8,2020"
+    },
+  ]
+
+
+  tasks= [{
+    taskid: 100,
+    assignee: 'Admin',
+    dueDate: '20-10-200',
+    status: 'done',
+    desc: 'Fill the form'
+
+  }, {
+    taskid: 101,
+    assignee: 'Admin',
+    dueDate: '25-10-2020',
+    status: 'not completed',
+    desc: 'COmplete the code'
+
+  },
+    {
+      taskid: 200,
+      assignee: 'Admin',
+      dueDate: '20-10-200',
+      status: 'done',
+      desc: 'water the plants'
+
+    }, {
+      taskid: 201,
+      assignee: 'Admin',
+      dueDate: '25-10-2020',
+      status: 'not completed',
+      desc: 'Wash the car'
+
+    }
+  ]
 
 
 }
