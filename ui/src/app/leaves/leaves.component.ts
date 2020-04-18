28ace0a44d6bb4224c527ed3724ea8f41a9e0996
Naveen <naveenyadav6256@gmail.com>
2020-04-18 15:24:52 +0530

    [Naveen] Refactor. removes unncessary code from leaves component

--- ui/src/app/leaves/leaves.component.ts
+++ ui/src/app/leaves/leaves.component.ts
@@ -3,7 +3,6 @@ import { PrincipleService } from './../util/principle.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute } from '@angular/router';
-import { NotificationsComponent } from '../notifications/notifications.component';
 import { NotificationService} from '../common/services/notification.service';
 
 
@@ -14,98 +13,78 @@ import { NotificationService} from '../common/services/notification.service';
 })
 export class LeavesComponent implements OnInit {
   public command:string;
-  public name:string='Naveen Gaddi'
   public count:number=0;
   public page:number =1;
   public remainingLeaves:number=21;
   leaves = [];
   pageOfItems: Array<any>;
-  constructor(private activeRoute:ActivatedRoute,private notificationService:NotificationService,private leavesservice:LeavesService,public principle:PrincipleService) { }
-  
 
-  
   public dateForm=new FormGroup({
     dateLeave:new FormControl('',Validators.required),
     reason:new FormControl('',Validators.required)
   })
-
-  get reason(){
-    return this.dateForm.get('reason');
-  }
-  get dateLeave(){
-    return this.dateForm.get('dateLeave');
-  }
-  mssg:boolean;
-  onConfirm()
-  {
-    
-    console.log(this.dateForm.get('dateLeave').value )
-    console.log(this.dateForm.get('reason').value)
-    this.notificationService.showSuccess("Leave Application Submitted!!!");
-  }
-
-  //taking messages from notification component
- notifications = new NotificationsComponent(this.notificationService);
-
- leavesMessage = this.notifications.leavesMessage
-
-
+  
+  constructor(private activeRoute:ActivatedRoute,private notificationService:NotificationService,private leavesService:LeavesService,public principle:PrincipleService) { }
   ngOnInit(): void {
       this.activeRoute.paramMap.subscribe(params=>{
           this.command = params.get('command');
-          console.log(this.command);
-        
-      })
-      this.leaves = [
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave'},
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave'},
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-        {date: '01-April-2020', status : 'Approved By Admin',reason:'Casual Leave'},
-        {date: '23-April-2020', status : 'Pending', reason:'Casual leave'},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-        {date: '25-April-2020', status : 'Approved By Admin', reason:'Emergency Leave',},
-        ];
-        this.add_leaves();
-
+          this.getAllLeaves();
+      });    
+  }  
+  get reason(){
+    return this.dateForm.get('reason');
   }
+  
+  get dateLeave(){
+    return this.dateForm.get('dateLeave');
+  }
+
+  onConfirm()
+  {
+    let data = {
+      "email" : this.principle.getUsername(),
+      "date" : this.dateForm.get('dateLeave').value,
+      "reason" : this.dateForm.get('reason').value
+    }
+    this.leavesService.requestLeave(data).subscribe(
+      res=>{
+        this.notificationService.showSuccess("Leave Application Submitted!!!");
+      },
+      err=>{
+        this.notificationService.showFailed("Something went wrong!");
+    });
+    
+  }
+
   onChangePage(pageOfItems: Array<any>) {
     // update current page of items
     this.pageOfItems = pageOfItems;
-}
-public message:any;
- add_leaves(){
-  let body={
-    email:this.principle.getUsername(),
-    reason:this.reason,
-    date:this.dateLeave
-
   }
-  let user_leaves_json=JSON.stringify(body);
-  this.leavesservice.add_leaves(user_leaves_json).subscribe(
-    res=>{
-      console.log(res)
-      this.message=res;
-      
-    },
-    err=>{
-      console.log(err)
-    }
-  )
-}
- 
+
+  getAllLeaves(){
+      this.leavesService.getAllLeaves().subscribe(
+        res=>{
+              if(res["status"] == 400){
+                //no leaves found////
+              }else{
+                this.appendLeaves(res);
+              }
+        },err=>{
+             console.log(err);
+      });;
+  }
+
+  appendLeaves(res){
+      let leavesData = Object.entries(res); 
+      this.leaves = [];     
+      for(let index = 0; index<leavesData.length;index++){
+          this.leaves.push(leavesData[index][1]);
+          if(!this.leaves[index].approved){
+            this.leaves[index].status = "Pending";
+          }else{
+            this.leaves[index].status = "Approved";
+          }
+      }
+  }
 
 }
--- ui/src/app/leaves/leaves.service.ts
+++ ui/src/app/leaves/leaves.service.ts
@@ -1,23 +1,20 @@
 import { GlobalsService } from './../common/services/globals.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PrincipleService } from './../util/principle.service';
-
 
 @Injectable({
     providedIn:'root'
 })
 
 export class LeavesService{
-    constructor(private http:HttpClient,private globalservice:GlobalsService){}
+    constructor(private http:HttpClient,private globalService:GlobalsService){}
     
-    public endpoint:string=this.globalservice.baseApiUrl;
-    
-        add_leaves(body){
-            return this.http.post(this.endpoint+'leaves/new',body)
-        
+    requestLeave(body){
+    	return this.http.post(this.globalService.baseApiUrl+'leaves/new',body);
+    }
 
-
+    getAllLeaves(){
+    	return this.http.post(this.globalService.baseApiUrl+'leaves/findAll',{});
     }
 }
 
