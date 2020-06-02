ba5e16942684c1a0716edb137381fff8ca0c85ef
Rishabh1106 <rishabhnagar11061999@gmail.com>
2020-06-02 09:27:45 +0530

    [Rishabh]Refactor. styling of timesheets.

--- ui/src/app/timesheets/timesheets.component.ts
+++ ui/src/app/timesheets/timesheets.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {TimesheetsService} from './timesheets.service';
-import {PrincipleService} from '../util/principle.service';
-import { NotificationService} from '../common/services/notification.service';
+import { TimesheetsService } from './timesheets.service';
+import { PrincipleService } from '../util/principle.service';
+import { NotificationService } from '../common/services/notification.service';
+import { AccountServiceService } from '../common/services/account-service.service';
+import { DropdownModule } from 'primeng/dropdown';
+import { SelectItem, MessageService } from 'primeng/api';
+
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -10,168 +14,194 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./timesheets.component.css']
 })
 export class TimesheetsComponent implements OnInit {
-  public today:Date = new Date();
-  public day:any;
-  public date:any;
-  public month:any;
-  public year:any;
-  public message :any;
-  public in_time:any;
-  public out_time:any;
-  public id:any;
-  public username:string;
-  public isSubmitted:boolean = false;
-  public isAdmin:boolean=false;
+  public today: Date = new Date();
+  public day: any;
+  public date: any;
+  public month: any;
+  public year: any;
+  public message: any;
+  public in_time: any;
+  public out_time: any;
+  public id: any;
+  public username: string;
+  public isSubmitted: boolean = false;
+  public isAdmin: boolean = false;
   public timings = new FormGroup({
-    in_hours : new FormControl('',Validators.required),
-    in_minutes : new FormControl('',Validators.required),
-    out_hours : new FormControl('',Validators.required),
-    out_minutes : new FormControl('',Validators.required),
+    in_hours: new FormControl('', Validators.required),
+    in_minutes: new FormControl('', Validators.required),
+    out_hours: new FormControl('', Validators.required),
+    out_minutes: new FormControl('', Validators.required),
 
   });
-  constructor(private principle:PrincipleService,private notificationService:NotificationService, private timesheetsService:TimesheetsService) { }
+  constructor(private principle: PrincipleService, private notificationService: NotificationService, private timesheetsService: TimesheetsService, private accountService: AccountServiceService) { }
 
   ngOnInit(): void {
-      this.updateCurrentDate(this.today.getFullYear(),this.today.getMonth(),this.today.getDate(),this.today.getDay())
-      this.fetchSheet(this.principle.getUsername(),this.date,this.month,this.year);
-      //admin view
-      if(this.principle.getRole() =="ADMIN"){
-          this.isAdmin = true;
-      }
+    this.updateCurrentDate(this.today.getFullYear(), this.today.getMonth(), this.today.getDate(), this.today.getDay())
+    this.fetchSheet(this.principle.getUsername(), this.date, this.month, this.year);
+    //admin view
+    if (this.principle.getRole() == "ADMIN") {
+      this.isAdmin = true;
+    }
+
+    this.getEmployeeData()
   }
-  findRecord(username,date,month,year){
-    console.log(username,date,month,year)
+  findRecord(username, date, month, year) {
+    console.log(username, date, month, year)
   }
-  onDateSelect(selectedDate){
+  onDateSelect(selectedDate) {
     //console.log(selectedDate)
-    let date = new Date(selectedDate.year,selectedDate.month-1,selectedDate.day);
-    this.updateCurrentDate(date.getFullYear(),date.getMonth(),date.getDate(),date.getDay());
+    let date = new Date(selectedDate.year, selectedDate.month - 1, selectedDate.day);
+    this.updateCurrentDate(date.getFullYear(), date.getMonth(), date.getDate(), date.getDay());
   }
-  updateCurrentDate(year,month,date,day){
+  updateCurrentDate(year, month, date, day) {
     //console.log(year,month,date,day)
     this.year = year;
     this.month = this.getMonth(month);
     this.date = date;
     this.day = this.getDay(day);
   }
-  fetchSheet(username,date,month,year){
-      let data = {"username":username || this.principle.getUsername(), "date": date, "month": month, "year": year}
-      this.timesheetsService.findTimeSheet(JSON.stringify(data)).subscribe(
-        res =>{
-          console.log("message",res)
-          this.message = res["message"];
-          if(res["data"]){
-            this.id = res["data"]["id"];
-            this.timings.patchValue({"in_hours": res["data"]["timings"]["in_time"]["hours"]});
-            this.timings.patchValue({"in_minutes": res["data"]["timings"]["in_time"]["minutes"]});
-            this.timings.patchValue({"out_hours": res["data"]["timings"]["out_time"]["hours"]});
-            this.timings.patchValue({"out_minutes": res["data"]["timings"]["out_time"]["minutes"]});
-            this.isSubmitted = true;
-          }else{
-            this.id = undefined;
-            this.timings.reset();
-          }
-        },err =>{
-          console.log(err)
+  fetchSheet(username, date, month, year) {
+    let data = { "emp_id": username || this.principle.getUsername(), "date": date, "month": month, "year": year }
+    this.timesheetsService.findTimeSheet(JSON.stringify(data)).subscribe(
+      res => {
+        console.log("message", res)
+        this.message = res["message"];
+        if (res["data"]) {
+          this.id = res["data"]["id"];
+          this.timings.patchValue({ "in_hours": res["data"]["timings"]["in_time"]["hours"] });
+          this.timings.patchValue({ "in_minutes": res["data"]["timings"]["in_time"]["minutes"] });
+          this.timings.patchValue({ "out_hours": res["data"]["timings"]["out_time"]["hours"] });
+          this.timings.patchValue({ "out_minutes": res["data"]["timings"]["out_time"]["minutes"] });
+          this.isSubmitted = true;
+        } else {
+          this.id = undefined;
+          this.timings.reset();
         }
-      );
+      }, err => {
+        console.log(err)
+      }
+    );
   }
 
-  saveTimeSheet(){
+  saveTimeSheet() {
 
     let data = {
-               "username":this.username || this.principle.getUsername(),
-               "day":this.day,
-               "date":this.date,
-               "month":this.month,
-               "year":this.year,
-               "timings":{
-                        "in_time":{"hours":this.timings.get('in_hours').value,"minutes":this.timings.get('in_minutes').value},
-                        "out_time":{"hours":this.timings.get('out_hours').value,"minutes":this.timings.get('out_minutes').value}
-                      },
-               "createdOn":this.today
-             };
+      "username": this.username || this.principle.getUsername(),
+      "day": this.day,
+      "date": this.date,
+      "month": this.month,
+      "year": this.year,
+      "timings": {
+        "in_time": { "hours": this.timings.get('in_hours').value, "minutes": this.timings.get('in_minutes').value },
+        "out_time": { "hours": this.timings.get('out_hours').value, "minutes": this.timings.get('out_minutes').value }
+      },
+      "createdOn": this.today
+    };
     //console.log(data);
-    if(this.id == undefined){
-        this.timesheetsService.addTimeSheet(data).subscribe(
-           res=>{
-              console.log(res);
-              if(res["status"] == 401){
-                this.notificationService.showFailed("Your account is not authorized!");
-              }
-           },err=>{
-               console.log(err)
-               this.notificationService.showFailed("Something went wrong!");
+    if (this.id == undefined) {
+      this.timesheetsService.addTimeSheet(data).subscribe(
+        res => {
+          console.log(res);
+          if (res["status"] == 401) {
+            this.notificationService.showFailed("Your account is not authorized!");
+          }
+        }, err => {
+          console.log(err)
+          this.notificationService.showFailed("Something went wrong!");
         });
-    }else{
-        this.timesheetsService.adminUpdateTimeSheet(data).subscribe(
-            res=>{
-               console.log(res);
-               if(res["status"] == 401){
-                this.notificationService.showFailed("Your account is not authorized!");
-              }
-            },err=>{
-                console.log(err)
-                 this.notificationService.showFailed("Something went wrong!");
-         });
+    } else {
+      this.timesheetsService.adminUpdateTimeSheet(data).subscribe(
+        res => {
+          console.log(res);
+          if (res["status"] == 401) {
+            this.notificationService.showFailed("Your account is not authorized!");
+          }
+        }, err => {
+          console.log(err)
+          this.notificationService.showFailed("Something went wrong!");
+        });
     }
   }
 
-  getDay(day){
+  getDay(day) {
     //let day = this.today.getDay();
-    switch(day){
+    switch (day) {
       case 0:
         return "Sunday";
       case 1:
-         return "Monday";
+        return "Monday";
       case 2:
-          return "Tuesday";
+        return "Tuesday";
       case 3:
-          return "Wednesday";
+        return "Wednesday";
       case 4:
-          return "Thursday";
+        return "Thursday";
       case 5:
-          return "Friday";
+        return "Friday";
       case 6:
-          return "Saturday";
+        return "Saturday";
     }
   }
 
-  getDate(){
+  getDate() {
     return this.today.getDate();
   }
 
-  getMonth(month){
-    switch(month){
-        case 0:
-            return "January";
-        case 1:
-            return "February";
-        case 2:
-            return "March";
-        case 3:
-            return "April";
-        case 4:
-            return "May";
-        case 5:
-            return "June";
-        case 6:
-            return "July";
-        case 7:
-            return "August";
-        case 8:
-            return "September";
-        case 9:
-            return "October";
-        case 10:
-            return "November";
-        case 11:
-            return "December";
+  getMonth(month) {
+    switch (month) {
+      case 0:
+        return "January";
+      case 1:
+        return "February";
+      case 2:
+        return "March";
+      case 3:
+        return "April";
+      case 4:
+        return "May";
+      case 5:
+        return "June";
+      case 6:
+        return "July";
+      case 7:
+        return "August";
+      case 8:
+        return "September";
+      case 9:
+        return "October";
+      case 10:
+        return "November";
+      case 11:
+        return "December";
     }
   }
 
-  getYear(){
+  getYear() {
     return this.today.getFullYear();
+  }
+
+  public employees: SelectItem[];
+
+  getEmployeeData() {
+    this.accountService.getUsers().subscribe(res => {
+      this.formatData(res)
+      //this.employees = res;
+
+    }, err => {
+      console.log(err);
+    });
+
+  }
+
+  formatData(res) {
+    //console.log(res)
+    this.employees = []
+    for (let i = 0; i < res.length; i++) {
+      this.employees.push(
+        { label: res[i].name, value: res[i].email }
+      )
+    }
+    console.log(this.employees)
   }
 
 }
