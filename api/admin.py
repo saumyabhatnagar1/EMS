23ebc30ac7f200e7316ce79791672569bd9953e2
Rishabh1106 <rishabhnagar11061999@gmail.com>
2020-06-16 12:39:49 +0530

    [Rishabh]Add.Notifications to admin

--- api/admin.py
+++ api/admin.py
@@ -5,6 +5,7 @@ from .models import Employee
 from .models import Leave, LeaveType
 from .models import Project, Task, WorksOn
 from .models import Timesheet
+from .models import Notification
 
 
 class AdminUser(admin.ModelAdmin):
@@ -18,3 +19,4 @@ admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(WorksOn)
 admin.site.register(Timesheet)
+admin.site.register(Notification)
