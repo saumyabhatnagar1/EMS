e5e10a1776851a47494958c9fe99b6657bf13815
Rishabh1106 <rishabhnagar11061999@gmail.com>
2020-06-18 13:04:49 +0530

    [Rishabh|Saumya]Add.Notice cards to notice-board and form refactored.

--- ui/src/app/notice-board/notice-board.component.ts
+++ ui/src/app/notice-board/notice-board.component.ts
@@ -1,67 +1,105 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { NoticeBoardService } from './notice-board.service';
-import {SelectItem,MessageService} from 'primeng/api';
+import { SelectItem, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-notice-board',
   templateUrl: './notice-board.component.html',
   styleUrls: ['./notice-board.component.css'],
-  providers:[MessageService],
+  providers: [MessageService],
 })
 export class NoticeBoardComponent implements OnInit {
 
-  constructor(private noticeService : NoticeBoardService,private messageService: MessageService) { }
-  public showCreateNotice:boolean=false
+  constructor(private noticeService: NoticeBoardService, private messageService: MessageService) {
+    this.typesArray = [
+      { label: 'Select Type', value: null },
+      { label: 'Broadcast', value: "broadcast" },
+      { label: 'Event', value: "event" },
+    ];
+  }
+  public showCreateNotice: boolean = false
 
   ngOnInit(): void {
     this.getNotices();
+    this.getNoticesByType('broadcast')
+    this.getNoticesByType('event')
+
   }
 
-  showDialog(){
+  showDialog() {
     this.showCreateNotice = true;
-    }
-    closeDialog(){
-      this.showCreateNotice=false
-    }
-  
-
-  public noticeForm=new FormGroup({
-    title:new FormControl(''),
-    description:new FormControl(''),
-    type:new FormControl('')
-  })
-addNewNotice(){
-  let data = {
-    'title' : this.noticeForm.get('title').value,
-    'description' : this.noticeForm.get('description').value,
-    'type' : this.noticeForm.get('type').value,
   }
-  this.noticeService.addNotice(data).subscribe(
-    res => {
-      console.log(res) 
-    this.messageService.add({severity:'success', summary:'Created!', detail:'New Notice Added.',life:5000});
-       this.getNotices()
-    },
-    err => {
-      console.log(err);
-      this.messageService.add({severity:'error',summary:'Failed',detail:'something went wrong in api',life:5000});
-    });
+  closeDialog() {
+    this.showCreateNotice = false
+  }
 
-}
-public notices:any=[];
-getNotices(){
-  this.noticeService.getAllNotices().subscribe(
-    res => {
-      console.log(res)
-      this.notices=res
-      console.log(this.notices)
-    },
-    err => {
-      console.log(err)
+
+  public noticeForm = new FormGroup({
+    title: new FormControl(''),
+    description: new FormControl(''),
+    type: new FormControl('')
+  })
+  addNewNotice() {
+    let data = {
+      'title': this.noticeForm.get('title').value,
+      'description': this.noticeForm.get('description').value,
+      'type': this.noticeForm.get('type').value,
     }
-  )
-}
+    this.noticeService.addNotice(data).subscribe(
+      res => {
+        console.log(res)
+        this.messageService.add({ severity: 'success', summary: 'Created!', detail: 'New Notice Added.', life: 5000 });
+        this.getNotices()
+        this.getNoticesByType('broadcast')
+        this.getNoticesByType('event')
+      },
+      err => {
+        console.log(err);
+        this.messageService.add({ severity: 'error', summary: 'Failed', detail: 'something went wrong in api', life: 5000 });
+      });
+
+  }
+  public notices: any = [];
+  getNotices() {
+    this.noticeService.getAllNotices().subscribe(
+      res => {
+        console.log(res)
+        this.notices = res
+        console.log(this.notices)
+
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }
+  public noticesByType: any = [];
+  broadcastNotices: any[];
+  eventNotices: any[];
+  public types: any = []
+
+  getNoticesByType(type) {
+    let data = {
+      type: type
+    }
+
+    this.noticeService.getNoticesByType(data).subscribe(
+      res => {
+        console.log(res)
+        this.noticesByType = res;
+        type == 'broadcast' ? this.broadcastNotices = this.noticesByType : this.eventNotices = this.noticesByType;
+      },
+      err => {
+        console.log(err)
+      }
+    )
+
+  }
+
+  public typesArray: SelectItem[];
+
+
 
 
 }
--- ui/src/app/notice-board/notice-board.service.ts
+++ ui/src/app/notice-board/notice-board.service.ts
@@ -1,28 +1,37 @@
 import { Injectable } from '@angular/core';
 import { PrincipleService } from './../util/principle.service';
 import { GlobalsService } from './../common/services/globals.service';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoticeBoardService {
 
-constructor(private principle:PrincipleService,private http:HttpClient,private globalService:GlobalsService) { }
+  constructor(private principle: PrincipleService, private http: HttpClient, private globalService: GlobalsService) { }
 
-addNotice(body){
-  const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-return this.http.post(this.globalService.baseApiUrl+'addNotice/',body,{headers:headers});
-}
+  addNotice(body) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'addNotice/', body, { headers: headers });
+  }
 
-getAllNotices(){
-  const headers=new HttpHeaders({
-    'Content-type':'application/json',
-    'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-})
-return this.http.get(this.globalService.baseApiUrl+'getAllNotices/',{headers:headers});
-}
+  getAllNotices() {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.get(this.globalService.baseApiUrl + 'getAllNotices/', { headers: headers });
+  }
+
+  getNoticesByType(data) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'getNoticesByType/', data, { headers: headers });
+  }
+
 }
