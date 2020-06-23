1380d35ceeda396cf9350d833a6419200bc733ef
Rishabh1106 <rishabhnagar11061999@gmail.com>
2020-06-23 10:03:27 +0530

    [Rishabh|Saumya|Sarthak]Refactor.add task only for project team employees

--- ui/src/app/projects/view-tasks/view-tasks.service.ts
+++ ui/src/app/projects/view-tasks/view-tasks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { GlobalsService } from './../../common/services/globals.service';
 import { PrincipleService } from 'src/app/util/principle.service';
 
@@ -8,52 +8,59 @@ import { PrincipleService } from 'src/app/util/principle.service';
 })
 export class ViewTasksService {
 
-  constructor(private http:HttpClient,private principle :PrincipleService,private globalService:GlobalsService) { }
+  constructor(private http: HttpClient, private principle: PrincipleService, private globalService: GlobalsService) { }
 
-  addTask(data){
+  addTask(data) {
 
-    const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-    return this.http.post(this.globalService.baseApiUrl+'tasks/new',data,{headers:headers})
-    
-  }
-
-  getTask(id){
-    const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-    return this.http.post(this.globalService.baseApiUrl+'tasks/getByProjectID',id,{headers:headers})
-  }
-  updateStatus(data){
-
-    const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-    return this.http.post(this.globalService.baseApiUrl+'tasks/update',data,{headers:headers})
-  }
-
-  addComment(data){
-      const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-  return this.http.post(this.globalService.baseApiUrl+'task/addComment',data,{headers:headers})
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'tasks/new', data, { headers: headers })
 
   }
 
-  getAllComments(data){
-       const headers=new HttpHeaders({
-      'Content-type':'application/json',
-      'Authorization':'Bearer '+this.principle.getItem('jwt_token')
-  })
-  return this.http.post(this.globalService.baseApiUrl+'task/getAllComments',data,{headers:headers})
+  getTask(id) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'tasks/getByProjectID', id, { headers: headers })
+  }
+  updateStatus(data) {
+
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'tasks/update', data, { headers: headers })
+  }
+
+  addComment(data) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'task/addComment', data, { headers: headers })
 
   }
 
-  
+  getAllComments(data) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'task/getAllComments', data, { headers: headers })
+
+  }
+
+  getTeam(data) {
+    const headers = new HttpHeaders({
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer ' + this.principle.getItem('jwt_token')
+    })
+    return this.http.post(this.globalService.baseApiUrl + 'project/getTeam', data, { headers: headers })
+  }
+
 
 }
